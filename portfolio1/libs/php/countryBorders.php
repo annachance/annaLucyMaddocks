c97ec64426aa97bf50142255fb0ad5c57f942a4a
annachance <91121694+annachance@users.noreply.github.com>
2022-09-21 12:08:21 +0100

    Update countryBorders.php

--- portfolio1/libs/php/countryBorders.php
+++ portfolio1/libs/php/countryBorders.php
@@ -5,27 +5,34 @@ error_reporting(E_ALL);
 
 $executionStartTime = microtime(true);
 
-$url='project1\libs\js\countryBorders.geo.json';
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL,$url);
+$result = file_get_contents("countryBorders.geo.json");
 
-$result=curl_exec($ch);
 
-curl_close($ch);
+  function compare($a, $b) {
+    if($a["name"] == $b["name"]) return 0;
+    return ($a["name"]<$B["name"]) ? -1 : 1;
+  }
 
-$decode = json_decode($result,true);    
+  $decode = json_decode($result, true);
+
+  $countries = [];
+
+  for ($i = 0; $i < count($decode['features']); $i++) {
+    array_push($countries, $decode['features'][$i]['properties']); 
+
+  };
+
+  usort($countries, "compare");
 
 $output['status']['code'] = "200";
 $output['status']['name'] = "ok";
 $output['status']['description'] = "success";
 $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-$output['data'] = $decode['']; 
+$output['data'] = $countries; 
 
 header('Content-Type: application/json; charset=UTF-8');
 
 echo json_encode($output); 
-
+ 
 ?>
