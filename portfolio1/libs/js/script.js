37818421b8d346abe341c0d8be662e744cd62503
annachance <91121694+annachance@users.noreply.github.com>
2022-09-23 15:31:26 +0100

    Update script.js

--- portfolio1/libs/js/script.js
+++ portfolio1/libs/js/script.js
@@ -1,50 +1,46 @@
 
-///////////////////////////////////////////////////////
+// Global Variables
+let countries = [];
+
+
 // Map initialization
+var map = L.map('map').setView([51.505, -0.09], 3);
 
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-// OSM layer
-
+// OSM/ Tile layer
 var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    // maxZoom: 19 ?!?!?! not sure if need this?1
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 osm.addTo(map);
 
-/*    these are currently in the html atm!!!!
-// World terrain
-
-var worldTerrain = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}', {
+ // World terrain
+var Esri_WorldTerrain = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: USGS, Esri, TANA, DeLorme, and NPS',
 	maxZoom: 13
 });
-worldTerrain.addTo(map); 
+Esri_WorldTerrain.addTo(map); 
 
 // World gray canvas
-
 var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
 	maxZoom: 16
 });
 Esri_WorldGrayCanvas.addTo(map); 
 
-/*
+
 // Icon
-var myIcon = L.icon({
+/*var myIcon = L.icon({
 	iconUrl: 'my-icon.png',
 	iconSize: [40, 40]
-}); 
+}); */
 
-// Marker - (example) need to amend!!!!!!!!!
-var singleMarker = L.marker([50.5, 30.5], /*{icon: myIcon}*/   /*);
+// Marker 
+var singleMarker = L.marker([50.5, 30.5], /*{icon: myIcon}*/ );
 var popup = singleMarker.bindPopup('This is my marker/popup!!').openPopup();
 popup.addTo(map); 
 
 
-/* .addTo(map);   ?!?!?!?!! */ 
+// .addTo(map);   ?!?!?!?!! 
 
-/*
 // Layer control
 var baseMaps = {
 	"OSM": osm,
@@ -55,50 +51,75 @@ var overlayMaps = {
 	"Marker": singleMarker
 };
 L.control.layers(baseMaps, overlayMaps).addTo(map);
-*/
 
-///////////////////////////////////////////////////////////
-     
+// Current user location marker (note sure if this is needed as of yet as may have to be onload not a button!!)
+L.control.locate().addTo(map);
+
+
+/////////////////////////////////////////////////////////////////////////
+
+// On webpage Load 
+$(document).ready(function () {
+    
+    $.ajax({ // Retrieve country names
+    
+        url: "libs/php/countryBorders.php",
+        type: 'GET',
+        dataType: 'json',
+        
+        success: function(result) {
+                   
+        if (result.status.name == "ok") {
+            var select = document.getElementById("country-dropdown");
+           console.log(result['data'])
+           for(var i = 0; i < result['data'].length; i++){
+            var opt = result['data'][i]['name'];
+            var val = result['data'][i]['code'];
+            var el = document.createElement("option");
+            el.textContent = opt;
+            el.value = val;
+            select.appendTo(el); 
+           }
+           
+        }
+        
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            // your error code
+            console.log(errorThrown)
+        } 
+        
+            }) 
+        });
+
+    ///////////////////////////////////////////////////////////
+  
+ // Adding countryborder.geo.json file to mapp (country borders)
+/* $.getJSON("libs/php/countryBorders.geo.json", function(data) {
+	L.geoJSON(data, {
+		style: function (feature) {
+			return {
+				color: 'black',
+				fillColor: 'none'
+			};
+		}
+	}).addTo(map);
+ }); */
+
+
+
+ ////////////////////////////////////////////////////
+
+ 
+  ///////////////////////////////////////////////////////////
+   /*  
   // not sure if need atm!!!
-/*
-$(window).on('load', function () {
+ 
+ $(window).on('load', function () {
 	if ($('#preloader').length) {
 	$('#preloader').delay(1000).fadeOut('slow', function () {
 	$(this).remove();
 	});
 	}
-});  */
-
-
-
- /* $('#btnRunCountry').click(function() {
-
-    $.ajax({
-        url: "libs/php/getCountryInfo.php",
-        type: 'POST',
-        dataType: 'json',
-        data: {
-            country: $('#selCountry').val(),
-            lang: $('#selLanguage').val()
-        },
-        success: function(result) {
-
-            //console.log(JSON.stringify(result));
-
-            if (result.status.name == "ok") {
-
-                $('#txtContinent').html(result['data'][0]['continent']);
-                $('#txtCapital').html(result['data'][0]['capital']);
-                $('#txtLanguages').html(result['data'][0]['languages']);
-                $('#txtPopulation').html(result['data'][0]['population']);
-                $('#txtArea').html(result['data'][0]['areaInSqKm']); 
-
-            }
-        
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            // your error code
-        }
-    }); 
-
-});  */
+  });   
+*/
