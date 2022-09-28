7f085b8dcffe94e9c7923d90a66c0dfa02c6172d
annachance <91121694+annachance@users.noreply.github.com>
2022-09-28 17:05:05 +0100

    Update script.js

--- portfolio1/libs/js/script.js
+++ portfolio1/libs/js/script.js
@@ -101,47 +101,40 @@ $(document).ready(function () {
         /////////////////////////////////////////////////////////////////////////
 // Retrieve country borders from geojson file
 
-$('#country-dropdown').click(function() {
-             let name = $('#country-dropdown').val();
-        
-            $.ajax({
-                url: "libs/php/countryBorders.php",
-                type: 'POST',
-                dataType: 'json',
-        
-                success: function(result) {
-                    console.log(result);
 
-                    if (result.status.name == "ok") {
-                    for (var i=0; i<result.data.border.features.length; i++) {
+var GeoJSONLayer = new L.GeoJSON();
 
+$('#country-dropdown').change(function() {
+    var iso_a2 = $('#country-dropdown option:selected').val();
+    $.ajax({
+        url: "libs/php/countryBorders.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            iso: iso_a2
+        },
+        success: function(result) {
+
+            if (result.status.name == "ok") {
+                for(var i = 0; i < result['data'].length; i++){
+        
                         if(result['data'][i]['properties']['iso_a2'] == document.getElementById("country-dropdown").value){
-                            var geojsonFeature = {"type":"FeatureCollection","features": [{"type":"Feature","properties":result['data'][i]['properties'],"geometry":result['data'][i]['geometry']}]};
+                            geojson = {"type":"FeatureCollection","features": [{"type":"Feature","properties":result['data'][i]['properties'],"geometry":result['data'][i]['geometry']}]};
                            
                         }
-
-                        $('#country-dropdown').append($('<option>', {
-                            value: result.data.border.features[i].properties.iso_a2,
-                            text: result.data.border.features[i].properties.name,
-                        }));
                     }
 
-                      border = L.geoJSON(geojsonFeature,{
-                          "type": type,
-                          "coordinates": coordinates
-                   },{
-                        style: {
-                          color: "black"
-                      }
-                   }).addTo(map); 
-                       map.fitBounds(border.getBounds())
-                    }
-            },
+                GeoJSONLayer.clearLayers();
+
+                var geoJSONFeature = result['data']['border'];
+                GeoJSONLayer.addData(geoJSONFeature).addTo(map);
+                map.fitBounds(GeoJSONLayer.getBounds());
+            }
+    
+        },
         error: function(jqXHR, textStatus, errorThrown) {
-            // your error code
             console.log(errorThrown),
             console.log(jqXHR);
-        } 
-        
-            })
-        });
+        }
+    })
+});
