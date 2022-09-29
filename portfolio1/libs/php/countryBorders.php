3ba484b2ba0ce0ea521ca2a6de4c88ffd0ee05d9
annachance <91121694+annachance@users.noreply.github.com>
2022-09-29 13:09:41 +0100

    Update countryBorders.php

--- portfolio1/libs/php/countryBorders.php
+++ portfolio1/libs/php/countryBorders.php
@@ -1,23 +1,30 @@
 <?php
 
- ini_set('display_errors', 'On');
- error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 
 $executionStartTime = microtime(true);
-    
-    $result = file_get_contents('countryBorders.geo.json');
+   
+   $result = file_get_contents('countryBorders.geo.json');
 
-    $border = json_decode($result,true);
+   $border = json_decode($result,true);
 
-    $output['status']['code'] = "200";
-    $output['status']['name'] = "ok";
-    $output['status']['description'] = "success";
-    $output['status']['executedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+   $countries = [];
 
-    $output['data']['border'] = $border;
-    
-    header('Content-Type: application/json; charset=UTF-8');
+  for ($i = 0; $i < count($border['features']); $i++) {
+    if ($border['features'][$i]['properties']['iso_a2'] == ) 
 
-    echo json_encode($output);
+  };
+
+   $output['status']['code'] = "200";
+   $output['status']['name'] = "ok";
+   $output['status']['description'] = "success";
+   $output['status']['executedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+
+   $output['data']['border'] = $border;
+   
+   header('Content-Type: application/json; charset=UTF-8');
+
+   echo json_encode($output);
 
 ?>  
