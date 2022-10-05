bcd532b16817af453544e0c232a0e82e104a6d84
annachance <91121694+annachance@users.noreply.github.com>
2022-10-05 11:51:16 +0100

    Update currentUserLocation.php

--- portfolio1/libs/php/currentUserLocation.php
+++ portfolio1/libs/php/currentUserLocation.php
@@ -1,31 +1,39 @@
 <?php
 
-    ini_set('display_errors', 'On');
-    error_reporting(E_ALL);
-    
-	$executionStartTime = microtime(true) / 1000;
-    // Open Cage- Reverse Geocoding API
-    $url = 'https://api.opencagedata.com/geocode/v1/json?q=' . $_REQUEST['lat'] . '+' . $_REQUEST['lng'] . '&key=12c285c4a085423d82d962afb2cffa4b';
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_URL,$url);
+$executionStartTime = microtime(true);
+//Geonames- Country Codes API
+$url='http://api.geonames.org/countryCode?lat=47.03&lng=10.2' . $_REQUEST['lat'] . '+' . $_REQUEST['lng'] . '&country=' . $_REQUEST['country'] . '&username=anna_chance&style=full';
 
-	$result=curl_exec($ch);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL,$url);
 
-	curl_close($ch);
+$result=curl_exec($ch);
 
-	$decode = json_decode($result,true);	
+curl_close($ch);
 
-	$output['status']['code'] = "200";
-	$output['status']['name'] = "ok";
-	$output['status']['description'] = "success";
-	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-	$output['data'] = $decode['results'];
-	
-	header('Content-Type: application/json; charset=UTF-8');
+$decode = json_decode($result,true);	
 
-	echo json_encode($output); 
+$countryCode;
 
-?>
+for ($i = 0; $i < count($decode['components']); $i++) {
+  if ($decode['components'][$i]['lat']['lng'] == $_REQUEST['ISO_3166-1_alpha-2']) {
+	$countryCode = $decode['components'][$i]['country'];
+  } 
+};
+
+$output['status']['code'] = "200";
+$output['status']['name'] = "ok";
+$output['status']['description'] = "success";
+$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+$output['data'] = $decode['country'];
+
+header('Content-Type: application/json; charset=UTF-8');
+
+echo json_encode($output); 
+
+?> 
