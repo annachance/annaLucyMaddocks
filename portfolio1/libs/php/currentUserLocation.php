cb4a4e2ab3432c51819b725f9155c50c91280afe
annachance <91121694+annachance@users.noreply.github.com>
2022-10-10 14:36:38 +0100

    Update currentUserLocation.php

--- portfolio1/libs/php/currentUserLocation.php
+++ portfolio1/libs/php/currentUserLocation.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 
 $executionStartTime = microtime(true);
 //Geonames- Country Codes API
-$url='http://api.geonames.org/countryCode?lat=47.03&lng=10.2' . $_REQUEST['lat'] . '+' . $_REQUEST['lng'] . '&country=' . $_REQUEST['country'] . '&username=anna_chance&style=full';
+$url='http://api.geonames.org/countryCodeJSON?lat='.$_REQUEST['lat'].'&lng='.$_REQUEST['lng'].'&username=anna_chance&style=full';
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -18,19 +18,11 @@ curl_close($ch);
 
 $decode = json_decode($result,true);	
 
-$countryCode;
-
-for ($i = 0; $i < count($decode['components']); $i++) {
-  if ($decode['components'][$i]['lat']['lng'] == $_REQUEST['ISO_3166-1_alpha-2']) {
-	$countryCode = $decode['components'][$i]['country'];
-  } 
-};
-
 $output['status']['code'] = "200";
 $output['status']['name'] = "ok";
 $output['status']['description'] = "success";
 $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-$output['data'] = $decode['country'];
+$output['data'] = $decode['countryCode'];
 
 header('Content-Type: application/json; charset=UTF-8');
 
