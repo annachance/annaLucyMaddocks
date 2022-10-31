9e2b4b1577b93c4295663f06773360ec0a02f879
annachance <91121694+annachance@users.noreply.github.com>
2022-10-31 14:14:08 +0000

    Update getAirports.php

--- portfolio1/libs/php/getAirports.php
+++ portfolio1/libs/php/getAirports.php
@@ -1,48 +1,52 @@
 <?php
 
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+$executionStartTime = microtime(true);
+
 $curl = curl_init();
 
-//"https://aerodatabox.p.rapidapi.com/airports/search/location/51.511142/-0.103869/km/100/20?withFlightInfoOnly=false",
-//"https://aerodatabox.p.rapidapi.com/airports/search/location/?lat=".$_REQUEST['lat'].'&lng='.$_REQUEST['lng']."km/100/20?withFlightInfoOnly=false",
-
-//** needs work!! */
-//AeroDataBox Rapid API- Airports! -  Works only on one lat&long coords in the url atm! need to get it to work for all!!!
+//AeroDataBox Rapid API- Airports
 curl_setopt_array($curl, [
-	CURLOPT_URL => 'https://aerodatabox.p.rapidapi.com/airports/search/location/'.$_REQUEST['lat'].'/'.$_REQUEST['lng'].'km/100/20?withFlightInfoOnly=false',
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_FOLLOWLOCATION => true,
-	CURLOPT_ENCODING => "",
-	CURLOPT_MAXREDIRS => 10,
-	CURLOPT_TIMEOUT => 30,
-	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	CURLOPT_CUSTOMREQUEST => "GET",
-	CURLOPT_HTTPHEADER => [
-		"X-RapidAPI-Host: aerodatabox.p.rapidapi.com",
-		"X-RapidAPI-Key: 5632b46c6emsh480a2114b4bf7ddp10d6f6jsn49fb514166b3"
-	],
+    CURLOPT_URL => 'https://aerodatabox.p.rapidapi.com/airports/search/location/'.$_REQUEST['lat'].'/'.$_REQUEST['lng'].'/km/100/20?withFlightInfoOnly=false',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => [
+        "X-RapidAPI-Host: aerodatabox.p.rapidapi.com",
+        "X-RapidAPI-Key: 5632b46c6emsh480a2114b4bf7ddp10d6f6jsn49fb514166b3"
+    ],
 ]);
 
 $response = curl_exec($curl);
+
 $err = curl_error($curl);
 
 curl_close($curl);
 
+$decode = json_decode($response);
+
 if ($err) {
-	echo "cURL Error #:" . $err;
+    $output['status']['code'] = "200";
+    $output['status']['name'] = "ok";
+    $output['status']['description'] = "success";
+    $output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+    $output['error'] = $err;
 } else {
-	echo $response;
+    $output['status']['code'] = "200";
+    $output['status']['name'] = "ok";
+    $output['status']['description'] = "success";
+    $output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+    $output['data'] = $decode;
 }
-
-$decode = json_decode($result,true);
-
-$output['status']['code'] = "200";
-$output['status']['name'] = "ok";
-$output['status']['description'] = "success";
-$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-$output['data'] = $decode["results"];
 
 header('Content-Type: application/json; charset=UTF-8');
 
-echo json_encode($output); 
+echo json_encode($output);
 
 ?>
