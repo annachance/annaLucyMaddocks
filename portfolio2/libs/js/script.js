f7337b530a93aba4647f3151b9d334abe2d0ab2a
annachance <91121694+annachance@users.noreply.github.com>
2022-12-20 18:38:57 +0000

    Update script.js

--- portfolio2/libs/js/script.js
+++ portfolio2/libs/js/script.js
@@ -10,6 +10,11 @@
 
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
+// Global Variables
+
+var data = [];
+
+///////////////////////////////////////////////////////////////////////////
 // On webpage Load 
 $(document).ready(function () {
 
@@ -22,15 +27,12 @@ $(document).ready(function () {
 	    
    	success: function(result) {
 
-        console.log(result);
-
         if (result.status.name == "ok") {
 
             console.log(result['data']);
 
-            for(var i = 0; i < result['data'].length; i++){
-
-                $("#employeeResultsData").append(result[data]);
+            const t = result.data;
+            updateEmployeeTable(t);
 
             }
           }
@@ -38,5 +40,107 @@ $(document).ready(function () {
         error: function(jqXHR, textStatus, errorThrown) {
             console.log(JSON.stringify(jqXHR, textStatus, errorThrown));
         }
+        }),
+	
+    $.ajax({
+
+        url:"libs/php/getAllDepartments.php",
+        type:"POST",
+        dataType:"json",
+
+        success: function(result) {
+
+        if (result.status.name == "ok") {
+
+            console.log(result['data']);
+
+            const t = result.data;
+            updateDepartmentTable(t)
+        }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(JSON.stringify(jqXHR, textStatus, errorThrown));
+        }
+	}),
+	
+     $.ajax({
+	
+        url:"libs/php/getAllLocations.php",
+        type:"POST",
+        dataType:"json",
+	
+        success:function(result) {
+
+        if (result.status.name == "ok") {
+
+            console.log(result['data']);
+
+            const t = result.data;
+            updateLocationTable(t)
+        }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(JSON.stringify(jqXHR, textStatus, errorThrown));
+        }
         })
 });
+
+const updateEmployeeTable = e => {
+
+    let t = "";
+    e.forEach(e => {
+
+        const a = getEmployeeRow(e);
+        t += a
+    }),
+        $("#employeeResultsData").html(t)
+    },
+        getEmployeeRow = e => {
+
+            const n = `<td>${e.firstName} ${e.lastName}</td>`,
+            j = `<td class="d-none d-lg-table-cell">${e.jobTitle}</td>`,
+            m = `<td class="d-none d-lg-table-cell">${e.email}</td>`,
+            d = `<td>${e.department}</td>`,
+            l = `<td class="d-none d-md-table-cell">${e.location}</td>`,
+            de = `<td><div class="d-flex justify-content-end"><button class="btn btn-outline-danger deleteEmployeeBtn" data-employee-id="${e.id}"><i class="fas fa-trash-alt"></i></button></div></td>`;
+            
+            return `<tr class="employeeRow" data-employee-id="${e.id}">${n}${j}${m}${d}${l}${de}</tr>`
+        };
+
+     updateDepartmentTable = e => {
+
+    let t = "";
+    e.forEach(e => {
+
+        const a = getDepartmentRow(e);
+        t += a
+    }),
+        $("#departmentResultsData").html(t)
+    },
+        getDepartmentRow = e => {
+
+            const n = `<td>${e.name}</td>`,
+            j = `<td>${e.location}</td>`,
+            m = `<td><div class="d-flex justify-content-end"><button class="btn btn-outline-danger deleteDepartmentBtn" data-department-id="${e.id}"><i class="fas fa-trash-alt"></i></button></div></td>`;
+
+            return `<tr class="departmentRow" data-department-id="${e.id}">${n}${j}${m}</tr>`
+        };
+            
+     updateLocationTable = e => {
+
+    let t = "";
+    e.forEach(e => {
+
+    const a = getLocationRow(e);
+    t += a
+}),
+    $("#locationResultsData").html(t)
+},
+    getLocationRow = e => {
+
+        const n = `<td>${e.name}</td>`,
+        dl = `<td><div class="d-flex justify-content-end"><button class="btn btn-outline-danger deleteLocationBtn" data-location-id="${e.id}"><i class="fas fa-trash-alt"></i></button></div></td>`;
+
+        return`<tr class="locationRow" data-location-id="${e.id}">${n}${dl}</tr>`
+    };
+
