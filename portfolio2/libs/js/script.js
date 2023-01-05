8dd89b55a4669ddc26019faaf960fd93d1d9e08b
annachance <91121694+annachance@users.noreply.github.com>
2023-01-05 13:29:18 +0000

    Update script.js

--- portfolio2/libs/js/script.js
+++ portfolio2/libs/js/script.js
@@ -13,19 +13,20 @@
 // Global Variables
 
 var data = [];
+let appTable = "Employee";
 
 ///////////////////////////////////////////////////////////////////////////
-// On webpage Load 
-$(document).ready(function () {
+// On Webpage Load 
+const refreshPersonnel = () => {
 
     // Retrieve EMPLOYEE DATABASE to application/ table
     $.ajax({
 
-        url:"libs/php/getAll.php",
-        type:"POST",
-        dataType:"json",
-	    
-   	success: function(result) {
+        url: "libs/php/getAll.php",
+        type: "POST",
+        dataType: "json",
+
+    success: function(result) {
 
         if (result.status.name == "ok") {
 
@@ -34,20 +35,23 @@ $(document).ready(function () {
             const t = result['data'];
             updateEmployeeTable(t);
 
-            }
-          }
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.log(JSON.stringify(jqXHR, textStatus, errorThrown));
         }
-        }),
-	
+        },
+        error: function(e, t, a) {
+            console.log("Error getAll"),
+            console.log(e.responseText),
+            console.log(`${t} : ${a}`)
+        }
+    })
+},
+
+refreshDepartments = () => {
     // Retrieve DEPARTMENTS DATABASE to application/ table
     $.ajax({
 
-        url:"libs/php/getAllDepartments.php",
-        type:"POST",
-        dataType:"json",
+        url: "libs/php/getAllDepartments.php",
+        type: "POST",
+        dataType: "json",
 
         success: function(result) {
 
@@ -56,37 +60,136 @@ $(document).ready(function () {
             console.log(result['data']);
 
             const t = result['data'];
+            populateDepartmentSelects(t),
             updateDepartmentTable(t)
         }
         },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.log(JSON.stringify(jqXHR, textStatus, errorThrown));
+        error:function(e, t, a) {
+            console.log("Error getAllDepartmentS"),
+            console.log(e.responseText),
+            console.log(`${t} : ${a}`)
         }
-	}),
-	
-     // Retrieve LOCATIONS DATABASE to application/ table
-     $.ajax({
-	
-        url:"libs/php/getAllLocations.php",
-        type:"POST",
-        dataType:"json",
-	
-        success:function(result) {
+    })
+},
+
+refreshLocations = () => {
+    // Retrieve LOCATIONS DATABASE to application/ table
+    $.ajax({
+
+        url: "libs/php/getAllLocations.php",
+        type: "POST",
+        dataType: "json",
+
+        success: function(result) {
 
         if (result.status.name == "ok") {
 
             console.log(result['data']);
 
             const t = result['data'];
+            populateLocationSelects(t),
             updateLocationTable(t)
         }
         },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.log(JSON.stringify(jqXHR, textStatus, errorThrown));
+        error: function(e, t, a) {
+            console.log("Error getAllLocationS"),
+            console.log(e.responseText),
+            console.log(`${t} : ${a}`)
         }
-        })
-});
+    })
+},
+  
+// POPULATE DROPDOWNS IN FORMS
+// DEPARTMENTS   
+populateDepartmentSelects = (e, t) => {
 
+    const a = t ? $(t) : $(".departmentSelect");
+    a.each(function() {
+
+        const t = $(this);
+        t.empty(),
+        id = t.attr("id"),
+
+        "addEmployeeDepartment" == id ? t.append('<option value="">Select Department</option>') : "searchDepartmentSelect" == id && t.append('<option value="">All Departments</option>'),
+
+        e.forEach(e => {
+            t.append(`<option value="${e.id}">${e.name}</option>`)
+        })
+    })
+},
+
+// LOCATIONS    
+populateLocationSelects = e => {
+
+    const t = $(".locationSelect");
+    t.each(function() {
+
+        const t = $(this);
+        t.empty(),
+        id = t.attr("id");
+
+        const a = "searchLocationSelect" == id ? "All Locations" : "addEmployeeLocation" == id ? t.append('<option value="">Select Location</option>') : "Select Location";
+        "editDepartmentLocation" != id && t.append(`<option value="">${a}</option>`),
+        
+        e.forEach(e => {
+            t.append(`<option value="${e.id}">${e.name}</option>`)
+        })
+    })
+};
+
+
+
+// **** ?!?!?!?!?!
+ $(".departmentSelect").change(function() {
+
+    const e = $(this).val(),
+    t = $(this).attr("id"),
+    a = "searchDepartmentSelect" == t ? $("#searchLocationSelect") : "addEmployeeDepartment" == t ? $("#addEmployeeLocation") : $("#editEmployeeLocation");
+    
+                    $.ajax({
+                        url: "libs/php/getLocationByID.php",
+                        type: "POST",
+                        dataType: "json",
+                        data: {id:e},
+
+                        success: function(e) {
+
+                            a.val(e.data.locationID)
+                        },
+                        error:function(e, t, a) {
+                            console.log("Error getLocationByDepartment"),
+                            console.log(e.responseText),
+                            console.log(`${t} : ${a}`)
+                        }
+            })
+    }),
+
+$("#searchLocationSelect").change(function() {
+    const e = $(this).val();
+
+    $.ajax({
+        url: "libs/php/getDepartmentByID.php",
+        type: "POST",
+        dataType: "json",
+        data: {id:e},
+
+        success: function(e) {
+
+            const t = e.data;
+            populateDepartmentSelects(t, "#searchDepartmentSelect")
+        },
+        error:function(e, t, a) {
+            console.log("Error getLocationsByDepartment"),
+            console.log(e.responseText),
+            console.log(`${t} : ${a}`)
+        }
+     })
+}); 
+//    **** ?!?!?!?!?!
+
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+// UPDATE/ SHOW TABLES ON APP
 const updateEmployeeTable = e => {
 
     let t = "";
@@ -107,12 +210,12 @@ const updateEmployeeTable = e => {
             de = `<td><div class="d-flex justify-content-end"><button class="btn btn-outline-danger deleteEmployeeBtn" data-employee-id="${e.id}"><i class="fas fa-trash-alt"></i></button></div></td>`;
             
             return `<tr class="employeeRow" data-employee-id="${e.id}">${n}${j}${m}${d}${l}${de}</tr>`
-        };
+        },
 
-     updateDepartmentTable = e => {
+updateDepartmentTable = e => {
 
-    let t = "";
-    e.forEach(e => {
+        let t = "";
+        e.forEach(e => {
 
         const a = getDepartmentRow(e);
         t += a
@@ -123,56 +226,115 @@ const updateEmployeeTable = e => {
 
             const n = `<td>${e.name}</td>`,
             j = `<td>${e.locationID}</td>`,
-            m = `<td><div class="d-flex justify-content-end"><button class="btn btn-outline-danger deleteDepartmentBtn" data-department-id="${e.id}"><i class="fas fa-trash-alt"></i></button></div></td>`;
+            de = `<td><div class="d-flex justify-content-end"><button class="btn btn-outline-danger deleteDepartmentBtn" data-department-id="${e.id}"><i class="fas fa-trash-alt"></i></button></div></td>`;
 
-            return `<tr class="departmentRow" data-department-id="${e.id}">${n}${j}${m}</tr>`
-        };
-            
-     updateLocationTable = e => {
-
-    let t = "";
-    e.forEach(e => {
-
-    const a = getLocationRow(e);
-    t += a
-}),
-    $("#locationResultsData").html(t)
+            return `<tr class="departmentRow" data-department-id="${e.id}">${n}${j}${de}</tr>`
 },
-    getLocationRow = e => {
+            
+updateLocationTable = e => {
+
+        let t = "";
+        e.forEach(e => {
+
+        const a = getLocationRow(e);
+        t += a
+    }),
+        $("#locationResultsData").html(t)
+    },
+        getLocationRow = e => {
 
         const n = `<td>${e.name}</td>`,
-        dl = `<td><div class="d-flex justify-content-end"><button class="btn btn-outline-danger deleteLocationBtn" data-location-id="${e.id}"><i class="fas fa-trash-alt"></i></button></div></td>`;
+        de = `<td><div class="d-flex justify-content-end"><button class="btn btn-outline-danger deleteLocationBtn" data-location-id="${e.id}"><i class="fas fa-trash-alt"></i></button></div></td>`;
 
-        return`<tr class="locationRow" data-location-id="${e.id}">${n}${dl}</tr>`
-    };
+        return`<tr class="locationRow" data-location-id="${e.id}">${n}${de}</tr>`
+};
+ 
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+// ADD BUTTON- Forms for Adding Employees/Departments/Locations
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+// SEARCH BUTTON 
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+// CLICK ON ROWS TO EDIT 
+
+// EDIT EMPLOYEES (when each row is clicked!!) 
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+// DELETE BUTTONS
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 // Nav Buttons- Update Tables for Employee/Department/Location 
 
-
     $("#nav-employees-tab").click(function() {
 
         updateEmployeeTable(t);
     }),
-     $("#navDepartment").click(function() {
-                                                                        
+     $("#nav-departments-tab").click(function() {
+                               
         updateDepartmentTable(t);
     }),
-    $("#navLocation").click(function() {
-	    
+    $("#nav-locations-tab").click(function() {
+
         updateLocationTable(t);
     });
 
+  
+   
+    
+
+
+
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////    
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
-// Page scrolls back to up to top of page when click the toTopButton
+// Page Scrolls Back To Up To Top Of Page When Click The toTopButton
 
-    const btn=$("#toTopButton");
+    const btn = $("#toTopButton");
 
-    btn.on("click",function(e) {
+    btn.on("click", function(e) {
         e.preventDefault(),
-        $("html, body").animate({scrollTop:0
+        $("html, body").animate({
+            scrollTop:0
         },
         "300")
-    });
+    }),
+    refreshPersonnel(),
+    refreshDepartments(),
+    refreshLocations();
