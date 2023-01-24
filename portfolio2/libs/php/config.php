d3f34aca4033cc61b1b1074e73a714d4d3423d50
annachance <91121694+annachance@users.noreply.github.com>
2023-01-24 12:33:02 +0000

    Update config.php

--- portfolio2/libs/php/config.php
+++ portfolio2/libs/php/config.php
@@ -1,7 +1,10 @@
 
 <?php
-
+    // Working!!!
 	// connection details for MySQL database
+	
+//////////////////////////////////////////////////////////////////////////////	
+	// localhost config
 
 	$cd_host = "127.0.0.1";
 	$cd_port = 3306;
@@ -11,6 +14,22 @@
 
 	$cd_dbname = "companydirectory";
 	$cd_user = "root";
-	$cd_password = "";
+	$cd_password = ""; 
+
+	
+
+
+//////////////////////////////////////////////////////////////////////////////	
+	// remote (website) config
+
+	/*$cd_host = "localhost";
+	$cd_port = 3306;
+	$cd_socket = "";
+
+	// database name, username and password
+
+	$cd_dbname = "annaofmz_companydirectory";
+	$cd_user = "annaofmz_anna_chance";
+	$cd_password = "yF+hx8NI;Wu)"; */
 
 ?>
