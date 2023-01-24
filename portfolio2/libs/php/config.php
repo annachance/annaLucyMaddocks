400191a066c00f542a57a961717631b39238cda8
annachance <91121694+annachance@users.noreply.github.com>
2023-01-24 13:18:29 +0000

    Add files via upload

---
+++ portfolio2/libs/php/config.php
@@ -0,0 +1,35 @@
+
+<?php
+    // Working!!!
+	// connection details for MySQL database
+	
+//////////////////////////////////////////////////////////////////////////////	
+	// localhost config
+
+	$cd_host = "127.0.0.1";
+	$cd_port = 3306;
+	$cd_socket = "";
+
+	// database name, username and password
+
+	$cd_dbname = "companydirectory";
+	$cd_user = "root";
+	$cd_password = ""; 
+
+	
+
+
+//////////////////////////////////////////////////////////////////////////////	
+	// remote (website) config
+
+	/*$cd_host = "localhost";
+	$cd_port = 3306;
+	$cd_socket = "";
+
+	// database name, username and password
+
+	$cd_dbname = "annaofmz_companydirectory";
+	$cd_user = "annaofmz_anna_chance";
+	$cd_password = "yF+hx8NI;Wu)"; */
+
+?>
