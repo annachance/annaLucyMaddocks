45e6eb679a57f948d67822393945dae52483b437
annachance <91121694+annachance@users.noreply.github.com>
2023-01-25 11:35:34 +0000

    Update getPersonnelCountByDepartment.php

--- portfolio2/libs/php/getPersonnelCountByDepartment.php
+++ portfolio2/libs/php/getPersonnelCountByDepartment.php
@@ -33,7 +33,7 @@ $query = $conn->prepare('SELECT COUNT(id)
 FROM personnel
 WHERE departmentID=?');
 
-$query->bind_param("i", $_REQUEST['id']);
+$query->bind_param("i", $_POST['id']);
 
 $query->execute();
 
@@ -75,7 +75,7 @@ $output['status']['name'] = "ok";
 $output['status']['description'] = "success";
 $output['status']['returnedIn'] = (microtime(true) -
 $executionStartTime) / 1000 . " ms";
-$output['data'] = $numEmployeeWithLoc;
+$output['data'] = $numEmployeeInDept;
 
 mysqli_close($conn);
 
